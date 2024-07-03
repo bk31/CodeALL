20e5272ba6df28bfbf6420b8c5477426cc86d3ef
BABU KATARI <kataribabu90@gmail.com>
2024-07-03 22:53:18 +0530

    Add project files.

---
+++ CodeALL/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CodeALL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Programs/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Programs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
