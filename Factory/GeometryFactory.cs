23e376ad406a85ff44aebf49dc6e9cd2152d760a
BABU KATARI <kataribabu90@gmail.com>
2024-07-03 23:40:33 +0530

    Refactored

---
+++ Factory/GeometryFactory.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factory
+{
+    public interface IGeometryFactory
+    {
+        string Draw();
+    }
+
+    public class Pentgon : IGeometryFactory
+    {
+        public string Draw()
+        {
+            return "PentAgon";
+        }
+    }
+    public class Hexagon : IGeometryFactory
+    {
+        public string Draw()
+        {
+            return "Hexagon";
+        }
+    }
+
+    public class GeometryFactory
+    {
+        public static IGeometryFactory GetGeometry(string name)
+        {
+            IGeometryFactory geometryFactory = null;
+            if (name.ToLower().Equals("pentgon"))
+            {
+                geometryFactory = new Pentgon();
+            }
+            else if (name.ToLower().Equals("hexagon"))
+            {
+                geometryFactory = new Hexagon();
+            }
+            else
+            {
+                Console.Write("Invalid Type" + name);
+            }
+            return geometryFactory;
+        }
+    }
+}
--- Factory/Program.cs
+++ Factory/Program.cs
@@ -1,10 +1,112 @@
-﻿namespace Factory
+﻿using System.Net.Http.Headers;
+
+namespace Factory
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //ICreditCard cardDetails = CreditCardFactory.GetCreditCard("HDFC");
+            //if (cardDetails != null)
+            //{
+            //    Console.WriteLine("CardType : " + cardDetails.GetCardType());
+            //    Console.WriteLine("CreditLimit : " + cardDetails.GetCreditLimit());
+            //    Console.WriteLine("AnnualCharge :" + cardDetails.GetAnnualCharge());
+            //}
+            //else
+            //{
+            //    Console.Write("Invalid Card Type");
+            //}
+
+            IGeometryFactory geometryFactory = GeometryFactory.GetGeometry("Hexagon");
+            if(geometryFactory != null)
+            {
+                Console.WriteLine("Shape : " + geometryFactory.Draw());
+            }
         }
+    }
+    public interface ICreditCard
+    {
+        string GetCardType();
+        int GetCreditLimit();
+        int GetAnnualCharge();
+    }
+    public class MoneyBack : ICreditCard
+    {
+        public int GetAnnualCharge()
+        {
+            return 500;
+        }
+
+        public string GetCardType()
+        {
+            return "MoneyBack";
+        }
+
+        public int GetCreditLimit()
+        {
+            return 15000;
+        }
+    }
+    public class IDFC : ICreditCard
+    {
+        public int GetAnnualCharge()
+        {
+            return 1500;
+        }
+
+        public string GetCardType()
+        {
+            return "IDFC";
+        }
+
+        public int GetCreditLimit()
+        {
+            return 10000;
+        }
+    }
+    public class HDFC : ICreditCard
+    {
+        public int GetAnnualCharge()
+        {
+            return 1000;
+        }
+
+        public string GetCardType()
+        {
+            return "HDFC";
+        }
+
+        public int GetCreditLimit()
+        {
+            return 150000;
+        }
+    }
+    public class CreditCardFactory
+    {
+
+        public static ICreditCard GetCreditCard(string cardType)
+        {
+            ICreditCard _creditCard;
+            if(cardType == "MoneyBack")
+            {
+                _creditCard= new MoneyBack();
+            }
+            else if(cardType == "IDFC")
+            {
+                _creditCard= new IDFC();
+            }
+            else if (cardType =="HDFC")
+            {
+                _creditCard = new HDFC();
+            }
+            else
+            {
+                throw new CannotUnloadAppDomainException();
+            }
+           return _creditCard;
+        }
+
+
     }
 }
